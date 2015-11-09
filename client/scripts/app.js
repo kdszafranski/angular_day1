76f76ea85fb1343c25c5df9e85f10df1955f80f2
Kris Szafranski <kdszafranski@gmail.com>
2015-11-09 14:01:21 -0600

    updated with working data binding

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,15 @@
 var myApp = angular.module('myApp', []);
+
+myApp.controller("welcomeController",['$scope', function($scope) {
+    // forward declaration
+    $scope.note = {};
+    $scope.nameArray = [];
+
+    $scope.clickButton = function() {
+        $scope.nameArray.push($scope.note);
+
+        console.log($scope.nameArray);
+
+        $scope.note = {};
+    };
+}]);
